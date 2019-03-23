63bee3761ce9bd910aa69335215f6f848fe2ebdc
EddyVinck <eddyvinck95@gmail.com>
2019-03-23 11:40:06 +0100

    items creation and prisma yoga flow

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // async items(parent, args, context, info) {
+  //   const items = await context.db.query.items();
+  //   return items;
+  // }
+  // the code above can be replaced with a prisma-binding if you just want to fetch data without any authentication
+  items: forwardTo("db")
+};
 
 module.exports = Query;
