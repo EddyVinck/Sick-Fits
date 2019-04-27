661a70f5cec92fe07e10ffcdde81fa766706497a
EddyVinck <eddyvinck95@gmail.com>
2019-04-28 00:23:27 +0200

    sign in back end & front end

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -83,6 +83,29 @@ const Mutations = {
 
     // Finally, return the user to the browser
     return user;
+  },
+  async signin(parent, args, context, info) {
+    const { email, password } = args;
+    // 1. Check if there is a user with that email
+    const user = await context.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid password");
+    }
+    // 3. Generate JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // 4. Set the cookie with the token
+    context.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24
+    });
+    // 5. Return the user
+    return user;
   }
 };
 
