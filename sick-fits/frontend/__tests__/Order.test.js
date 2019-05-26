c8a1763211b5c209d4189463a8ed7dfd2613bfee
EddyVinck <eddyvinck95@gmail.com>
2019-05-26 11:07:07 +0200

    clean up test file

--- sick-fits/frontend/__tests__/Order.test.js
+++ sick-fits/frontend/__tests__/Order.test.js
@@ -2,13 +2,8 @@ import { mount } from "enzyme";
 import wait from "waait";
 import toJSON from "enzyme-to-json";
 import { MockedProvider } from "react-apollo/test-utils";
-import { CURRENT_USER_QUERY } from "../components/User";
 import Order, { SINGLE_ORDER_QUERY } from "../components/Order";
-import { ApolloConsumer } from "react-apollo";
-import { fakeUser, fakeCartItem, fakeOrder } from "../lib/testUtils";
-import NProgress from "nprogress";
-import Error from "../components/ErrorMessage";
-import Router from "next/router";
+import { fakeOrder } from "../lib/testUtils";
 
 const order = fakeOrder();
 const mocks = [
@@ -40,7 +35,5 @@ describe("<Order />", () => {
     wrapper.update();
     expect(wrapper.find("li[data-test='order']").exists()).toBe(true);
     expect(toJSON(wrapper.find("li[data-test='order']"))).toMatchSnapshot();
-
-    console.log(wrapper.debug());
   });
 });
